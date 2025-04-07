92c6239d65b9d76000ece80f3d70934e2e5826f6
Bui Quang Trung <trungbq8@gmail.com>
2025-04-08 00:24:47 +0700

    Update deployment

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,15 @@
 # Must allow execute this mannually: chmod +x /root/openfund/deploy.sh
+
+# Activate virtual environment
+source /root/openfund/env/bin/activate
+
 # Pull the latest code
 git pull origin main
 
 # Reinstall dependencies
 pip install -r requirements.txt
+
+deactivate
 
 # Restart the app
 systemctl restart openfund.service
