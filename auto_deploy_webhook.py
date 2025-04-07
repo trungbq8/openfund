a78af1ec09012de3e3e8adba34223a3a00b886f0
Bui Quang Trung <trungbq8@gmail.com>
2025-04-07 23:49:22 +0700

    Update deployment

--- auto_deploy_webhook.py
+++ auto_deploy_webhook.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    os.system("cd /root/openfund")
-    os.system("chmod +x deploy.sh")
-    os.system("./deploy.sh")
+    os.system("chmod +x /root/openfund/deploy.sh")
+    os.system("/root/openfund/deploy.sh")
     return "Deployed", 200
 #
 if __name__ == "__main__":
