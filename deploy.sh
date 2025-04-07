cf2f284dca5dfd9aa65bec0a7d97668a85ed2df1
Bui Quang Trung <trungbq8@gmail.com>
2025-04-08 00:17:12 +0700

    Update deployment

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
+# Must allow execute the file mannually: chmod +x /root/openfund/deploy.sh
 # Pull the latest code
 git pull origin main
 
 # Reinstall dependencies
-#pip install -r requirements.txt
+pip install -r requirements.txt
 
 # Restart the app
-#systemctl restart openfund.service
+systemctl restart openfund.service
