8b84c4f9da3290eeaf42b8d1d2057d5a516823b1
Bui Quang Trung <trungbq8@gmail.com>
2025-04-11 11:34:41 +0700

    Update index.html

--- app.py
+++ app.py
@@ -4,8 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-   
    return render_template("index.html")
+
+@app.route("/invest")
+def invest():
+   return render_template("invest.html")
 
 if __name__ == "__main__":
    app.run(host="0.0.0.0", port=5555)
