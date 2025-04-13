a6c759e5b69e9c93c4de942d11bb87a7ee63fbc5
Bui Quang Trung <trungbq8@gmail.com>
2025-04-13 20:05:31 +0700

    Update requirements.txt

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ def invest():
 @app.route("/sign-up", methods=['GET', 'POST'])
 def signup():
    if request.method == "POST":
-      time.sleep(1)
       data = request.json
       first_name = data.get('first_name')
       last_name = data.get('last_name')
@@ -30,12 +29,11 @@ def signup():
       password = data.get('password')
       wallet_address = data.get('wallet_address')
       signature = data.get("signature")
-      print(signature)
 
       nonce = session.get('nonce')
       message = f"Sign this message to verify your wallet ownership with OpenFund. Nonce: {nonce}"
       message_hash = encode_defunct(text=message)
-
+      
       recovered_address = eth_account.Account.recover_message(message_hash, signature=signature)
       if recovered_address.lower() == wallet_address.lower():
          print("=== NEW USER SIGNUP ===")
@@ -47,7 +45,7 @@ def signup():
             
          return jsonify({"success": True, "message": "Account created successfully"})
       else:
-         return jsonify({"success": False, "message": f"{signature}"})
+         return jsonify({"success": False, "message": f"{recovered_address.lower()}"})
    else:
       nonce = secrets.token_hex(16)
       session['nonce'] = nonce
