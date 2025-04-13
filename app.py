db18c18bd4549029f9a5b9559a7077d1497aca9e
Bui Quang Trung <trungbq8@gmail.com>
2025-04-13 20:08:59 +0700

    Update requirements.txt

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def signup():
             
          return jsonify({"success": True, "message": "Account created successfully"})
       else:
-         return jsonify({"success": False, "message": f"{message}"})
+         return jsonify({"success": False, "message": f"{nonce}"})
    else:
       nonce = secrets.token_hex(16)
       session['nonce'] = nonce
